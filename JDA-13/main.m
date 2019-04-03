c7f1c31b06d9a092b871314b43e01d763b444b66
Sihan Zhu <43961541+WUzhusihan@users.noreply.github.com>
2019-04-03 18:55:40 +0800

    Update main.m

--- JDA-13/main.m
+++ JDA-13/main.m
@@ -1,12 +1,11 @@
 clear;
-%¶ÁÈ¡Ô´ÓòÊı¾İ#SUN->S1 Scene-15->S2 banjaluka->T1, UCMERCED->T2  C19->T3
 %caltech,amazon,webcam,dslr
 load('/data/dslr.mat');
 src_data =  feas; src_label = label;
-%¶ÁÈ¡Ä¿±êÓòÊı¾İ
+%è¯»å–ç›®æ ‡åŸŸæ•°æ®
 load('/data/webcam.mat');
 tar_data =  feas; tar_label = label;
-%ÉèÖÃ²ÎÊıkernal¿ÉÒÔÊÇrbf,linear,sam,primal, 
+%è®¾ç½®å‚æ•°kernalå¯ä»¥æ˜¯rbf,linear,sam,primal, 
 options.dim = 30;options.lambda = 1;
 options.gamma =1 ;options.kernel_type = 'primal';options.T = 10;
 [~,acc_ite,A] = myJDA(src_data,src_label,tar_data,tar_label,options);
