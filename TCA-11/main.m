f0ac4bf63c257ec0b25c154339c5eff64182e8a6
Sihan Zhu <43961541+WUzhusihan@users.noreply.github.com>
2019-04-03 18:52:29 +0800

    Update main.m

--- TCA-11/main.m
+++ TCA-11/main.m
@@ -1,18 +1,18 @@
 clear;
-%¶ÁÈ¡Ô´ÓòÊı¾İ#SUN->S1 Scene-15->S2 banjaluka->T1, UCMERCED->T2  C19->T3
+%è¯»å–æºåŸŸæ•°æ®#SUN->S1 Scene-15->S2 banjaluka->T1, UCMERCED->T2  C19->T3
 %caltech,amazon,webcam,dslr
 load('/data/dslr.mat');
 src_data =  feas; src_label = label;
-%¶ÁÈ¡Ä¿±êÓòÊı¾İ
+%è¯»å–ç›®æ ‡åŸŸæ•°æ®
 load('/data/caltech.mat');
 tar_data =  feas; tar_label = label;
-%ÉèÖÃ²ÎÊıkernal¿ÉÒÔÊÇrbf,linear,sam,primal, 
+%è®¾ç½®å‚æ•°kernalå¯ä»¥æ˜¯rbf,linear,sam,primal, 
 options.dim = 30;options.lambda = 1;
 options.gamma =1 ;options.kernel_type = 'primal';
-[X_src_new,X_tar_new,A] = TCA(src_data,tar_data,options);
+[X_src_new,X_tar_new,A] = myTCA(src_data,tar_data,options);
 % model = libsvmtrain(src_label, X_src_new,'-c 100');
 % [pred, acc, ~] = libsvmpredict(tar_label, X_tar_new, model);
 knn_model = fitcknn(X_src_new,src_label,'NumNeighbors',1);%Xs_new,Ys
 Y_tar_pseudo = knn_model.predict(X_tar_new);%Xt_new
 acc = length(find(Y_tar_pseudo==tar_label))/length(tar_label)
-%ÔËĞĞKNN·ÖÀàÆ÷
+%è¿è¡ŒKNNåˆ†ç±»å™¨
