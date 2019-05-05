18d40b78566bffe59ad682d824a2900315e3fa39
Sihan Zhu <43961541+WUzhusihan@users.noreply.github.com>
2019-05-05 11:00:59 +0800

    TJM code

---
+++ TJM-14/main.m
@@ -0,0 +1,12 @@
+clear;
+%读取源域数据#SUN->S1 Scene-15->S2 banjaluka->T1, UCMERCED->T2  C19->T3
+%caltech,amazon,webcam,dslr
+load('/data/caltech.mat');
+src_data =  feas; src_label = label;
+%读取目标域数据
+load('/data/webcam.mat');
+tar_data =  feas; tar_label = label;
+%设置参数kernal可以是rbf,linear,sam,primal, 
+options.dim = 30;options.lambda = 1;
+options.gamma =1 ;options.kernel_type = 'primal';options.T = 10;
+[acc,acc_list,A] = mTJM(src_data,src_label,tar_data,tar_label,options);
